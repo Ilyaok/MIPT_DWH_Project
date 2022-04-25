35022dd8c6a8199a413c1701b856a7bd14be12f0
ilyaok <Strategy2031>
2022-04-25 20:17:25 +0300

    DDL

--- sql_scripts/DDL.sql
+++ sql_scripts/DDL.sql
@@ -1,5 +1,7 @@
 -- Создание требуемых в задании таблиц
 
+-- Таблицы фактов
+
 -- Таблица фактов - транзакции
 CREATE TABLE DEMIPT2.GOLD_DWH_FACT_TRANSACTIONS (
     trans_id varchar2(30) primary key,
@@ -21,10 +23,47 @@ CREATE TABLE DEMIPT2.GOLD_DWH_FACT_PASSPORT_BLACKLIST (
     update_dt date
 );
 
+-- Таблицы измерений
+
 -- Таблица измерений - терминалы
 CREATE TABLE DEMIPT2.GOLD_DWH_DIM_TERMINALS (
     terminal_id varchar2(10) primary key,
     terminal_type varchar2(10),
     terminal_city varchar2(20),
-    terminal_address varchar2(100)
+    terminal_address varchar2(100),
+    create_dt date,
+    update_dt date
+);
+
+-- Таблица измерений - карты
+CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CARDS (
+    card_num varchar2(20) primary key,
+    account_num varchar2(50),
+    terminal_city varchar2(20),
+    terminal_address varchar2(100),
+    create_dt date,
+    update_dt date
+);
+
+-- Таблица измерений - аккаунты
+CREATE TABLE DEMIPT2.GOLD_DWH_DIM_ACCOUNTS (
+    account_num varchar2(50) primary key,
+    valid_to date,
+    client varchar2(10),
+    create_dt date,
+    update_dt date
+);
+
+-- Таблица измерений - клиенты
+CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CLIENTS (
+    client_id varchar2(10) primary key,
+    last_name varchar2(30),
+    first_name varchar2(30),
+    patronymic varchar2(30),
+    date_of_birth date,
+    passport_num varchar2(20),
+    passport_valid_to date,
+    phone varchar(20),
+    create_dt date,
+    update_dt date
 );
