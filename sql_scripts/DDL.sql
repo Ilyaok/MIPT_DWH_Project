2f651caf5e158205da7e330a1c647b261c536781
ilyaok <Strategy2031>
2022-04-26 11:55:23 +0300

    tables, pipelines

--- sql_scripts/DDL.sql
+++ sql_scripts/DDL.sql
@@ -1,9 +1,73 @@
 -- Создание требуемых в задании таблиц
 
+-- Стейджинговые таблицы
+
+-- Стейдж-таблица - транзакции
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_FACT_TRANSACTIONS (
+    trans_id varchar2(30) primary key,
+    trans_date date,
+    card_num varchar2(20),
+    oper_type varchar2(20),
+    amt decimal,
+    oper_result varchar2(20),
+    terminal varchar2(20),
+    create_dt date,
+    update_dt date
+);
+
+-- Стейдж-таблица - черный список паспортов
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_FACT_PASSPORT_BLACKLIST (
+    passport_num varchar2(20),
+    entry_dt date,
+    create_dt date,
+    update_dt date
+);
+
+-- Стейдж-таблица - терминалы
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_DIM_TERMINALS (
+    terminal_id varchar2(10) primary key,
+    terminal_type varchar2(10),
+    terminal_city varchar2(20),
+    terminal_address varchar2(100),
+    create_dt date,
+    update_dt date
+);
+
+-- Стейдж-таблица - карты
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_DIM_CARDS (
+    card_num varchar2(20) primary key,
+    account_num varchar2(50),
+    create_dt date,
+    update_dt date
+);
+
+-- Стейдж-таблица - аккаунты
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_DIM_ACCOUNTS (
+    account_num varchar2(50) primary key,
+    valid_to date,
+    client varchar2(10),
+    create_dt date,
+    update_dt date
+);
+
+-- Стейдж-таблица - клиенты
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_STG_DIM_CLIENTS (
+    client_id varchar2(10) primary key,
+    last_name varchar2(30),
+    first_name varchar2(30),
+    patronymic varchar2(30),
+    date_of_birth date,
+    passport_num varchar2(20),
+    passport_valid_to date,
+    phone varchar(20),
+    create_dt date,
+    update_dt date
+);
+
 -- Таблицы фактов
 
 -- Таблица фактов - транзакции
-CREATE TABLE DEMIPT2.GOLD_DWH_FACT_TRANSACTIONS (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_FACT_TRANSACTIONS (
     trans_id varchar2(30) primary key,
     trans_date date,
     card_num varchar2(20),
@@ -16,17 +80,18 @@ CREATE TABLE DEMIPT2.GOLD_DWH_FACT_TRANSACTIONS (
 );
 
 -- Таблица фактов - черный список паспортов
-CREATE TABLE DEMIPT2.GOLD_DWH_FACT_PASSPORT_BLACKLIST (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_FACT_PASSPORT_BLACKLIST_HIST (
     passport_num varchar2(20),
     entry_dt date,
-    create_dt date,
-    update_dt date
+    effective_from date,
+	effective_to date,
+	deleted_flg char(1)
 );
 
 -- Таблицы измерений
 
 -- Таблица измерений - терминалы
-CREATE TABLE DEMIPT2.GOLD_DWH_DIM_TERMINALS (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_DIM_TERMINALS_HIST (
     terminal_id varchar2(10) primary key,
     terminal_type varchar2(10),
     terminal_city varchar2(20),
@@ -36,7 +101,7 @@ CREATE TABLE DEMIPT2.GOLD_DWH_DIM_TERMINALS (
 );
 
 -- Таблица измерений - карты
-CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CARDS (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_DIM_CARDS_HIST (
     card_num varchar2(20) primary key,
     account_num varchar2(50),
     create_dt date,
@@ -44,7 +109,7 @@ CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CARDS (
 );
 
 -- Таблица измерений - аккаунты
-CREATE TABLE DEMIPT2.GOLD_DWH_DIM_ACCOUNTS (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_DIM_ACCOUNTS_HIST (
     account_num varchar2(50) primary key,
     valid_to date,
     client varchar2(10),
@@ -53,7 +118,7 @@ CREATE TABLE DEMIPT2.GOLD_DWH_DIM_ACCOUNTS (
 );
 
 -- Таблица измерений - клиенты
-CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CLIENTS (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_DWH_DIM_CLIENTS_HIST (
     client_id varchar2(10) primary key,
     last_name varchar2(30),
     first_name varchar2(30),
@@ -67,14 +132,14 @@ CREATE TABLE DEMIPT2.GOLD_DWH_DIM_CLIENTS (
 );
 
 -- Таблица метаданных (доработать с именами)
-CREATE TABLE DEMIPT2.GOLD_META_TABLENAME (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_META_BANK (
     table_db varchar2(30),
     table_name varchar2(30),
     last_update_dt date
 );
 
 -- Таблица-отчет по мошенническим операциям
-CREATE TABLE DEMIPT2.GOLD_REP_FRAUD (
+CREATE TABLE IF NOT EXISTS DEMIPT2.GOLD_REP_FRAUD (
     event_dt date,
     passport varchar2(20),
     fio varchar2(100),
